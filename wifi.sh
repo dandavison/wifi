ce0d6960ee1593666b90419d70c8b561d893f36d
Dan Davison <dan@counsyl.com>
2016-02-07 18:17:24 -0800

    Clean up

--- wifi.sh
+++ wifi.sh
@@ -1,11 +1,6 @@
 # http://blog.mattcrampton.com/post/64144666914/managing-wifi-connections-using-the-mac-osx
 
-
-# Turn off wifi on your macbook from the Mac OSX terminal command line:
-# networksetup -setairportpower en0 off
-# Turn on wifi on your macbook from the Mac OSX terminal command line:
-# networksetup -setairportpower en0 on
-
+# Turn wifi on/off
 wifi () {
     val=$1
     [ "$val" = "on" ] || [ "$val" = "off" ] || {
@@ -15,14 +10,12 @@ wifi () {
     networksetup -setairportpower en0 $val
 }
 
-# List available wifi networks from the Mac OSX terminal command line:
-# /System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan
-
+# List available wifi networks
 wifi-list () {
     /System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport scan
 }
 
-# Join a wifi network from the Mac OSX terminal command line:
+# Join a wifi network
 # networksetup -setairportnetwork en0 WIFI_SSID_I_WANT_TO_JOIN WIFI_PASSWORD
 
 wifi-join () {
